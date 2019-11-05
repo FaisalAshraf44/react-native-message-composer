2aed00a5e76b4c63f80e55945a46f3d8c9167a50
Eric Chang <echang@pared.com>
2019-11-05 13:12:05 -0800

    updated the RNMessageComposer.m file

--- RNMessageComposer/RNMessageComposer.m
+++ RNMessageComposer/RNMessageComposer.m
@@ -53,116 +53,121 @@ RCT_EXPORT_METHOD(messagingSupported:(RCTResponseSenderBlock)callback)
 
 RCT_EXPORT_METHOD(composeMessageWithArgs:(NSDictionary *)args callback:(RCTResponseSenderBlock)callback)
 {
-    // check the device can actually send messages - return from method if not supported
+            // check the device can actually send messages - return from method if not supported
     if(![MFMessageComposeViewController canSendText])
     {
         callback(@[@"notsupported"]);
         return;
     }
 
-    MFMessageComposeViewController *mcvc = [[MFMessageComposeViewController alloc] init];
-    mcvc.messageComposeDelegate = self;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        MFMessageComposeViewController *mcvc = [[MFMessageComposeViewController alloc] init];
+        mcvc.messageComposeDelegate = self;
 
-    if(args[@"recipients"])
-    {
-        // check that recipients was passed as an NSArray
-        if([args[@"recipients"] isKindOfClass:[NSArray class]])
+        if(args[@"recipients"])
         {
-            NSArray *recipients = args[@"recipients"];
-            if(recipients.count > 0)
+            // check that recipients was passed as an NSArray
+            if([args[@"recipients"] isKindOfClass:[NSArray class]])
             {
-                NSMutableArray *validRecipientTypes = [[NSMutableArray alloc] init];
+                NSArray *recipients = args[@"recipients"];
+                if(recipients.count > 0)
+                {
+                    NSMutableArray *validRecipientTypes = [[NSMutableArray alloc] init];
 
-                // Check type of each item in NSArray and only use it if it was provided as an NSString.
-                // We could be more lenient here and just use RCTConvert on all values even if not
-                // provided as NSString originally. For now I prefer being more strict.
-                for(id recipient in recipients)
-                {
-                    if([recipient isKindOfClass:[NSString class]])
+                    // Check type of each item in NSArray and only use it if it was provided as an NSString.
+                    // We could be more lenient here and just use RCTConvert on all values even if not
+                    // provided as NSString originally. For now I prefer being more strict.
+                    for(id recipient in recipients)
                     {
-                        [validRecipientTypes addObject:recipient];
+                        if([recipient isKindOfClass:[NSString class]])
+                        {
+                            [validRecipientTypes addObject:recipient];
+                        }
                     }
-                }
-                if(validRecipientTypes.count != 0)
-                {
-                    mcvc.recipients = validRecipientTypes;
+                    if(validRecipientTypes.count != 0)
+                    {
+                        mcvc.recipients = validRecipientTypes;
+                    }
+                    else
+                    {
+                        RCTLog(@"You provided a recipients array but it did not contain any valid argument types");
+                    }
                 }
                 else
                 {
-                    RCTLog(@"You provided a recipients array but it did not contain any valid argument types");
+                    RCTLog(@"You provided a recipients array but it was empty. No values to add");
                 }
             }
             else
             {
-                RCTLog(@"You provided a recipients array but it was empty. No values to add");
+                RCTLog(@"recipients must be supplied as an array. Ignoring the values provided");
             }
         }
-        else
+
+        // check to see if messages support subjects - if they do check if a subject has been supplied
+        if([MFMessageComposeViewController canSendSubject])
         {
-            RCTLog(@"recipients must be supplied as an array. Ignoring the values provided");
-        }
-    }
-
-    // check to see if messages support subjects - if they do check if a subject has been supplied
-    if([MFMessageComposeViewController canSendSubject])
-    {
-        if(args[@"subject"])
-        {
-            mcvc.subject = [RCTConvert NSString:args[@"subject"]];
-        }
-    }
-
-    if(args[@"messageText"])
-    {
-        mcvc.body = [RCTConvert NSString:args[@"messageText"]];
-    }
-
-    if(args[@"presentAnimated"])
-    {
-        presentAnimated = [RCTConvert BOOL:args[@"presentAnimated"]];
-    }
-
-    if(args[@"dismissAnimated"])
-    {
-        dismissAnimated = [RCTConvert BOOL:args[@"dismissAnimated"]];
-    }
-
-    if([MFMessageComposeViewController canSendAttachments]) {
-        if(args[@"attachments"])
-        {
-            if([args[@"attachments"] isKindOfClass:[NSArray class]])
+            if(args[@"subject"])
             {
-                NSArray *attachments = args[@"attachments"];
-                for(id attachment in attachments)
-                {
-                    if([attachment isKindOfClass:[NSDictionary class]])
-                    {
-                        if ([attachment objectForKey:@"url"] && [attachment objectForKey:@"typeIdentifier"])
-                        {
-                            NSURL *url = [NSURL URLWithString:[attachment objectForKey:@"url"]];
-                            NSString *typeIdentifier = [attachment objectForKey:@"typeIdentifier"];
-                            NSString *filename = [attachment objectForKey:@"filename"];
+                mcvc.subject = [RCTConvert NSString:args[@"subject"]];
+            }
+        }
 
-                            if (![mcvc addAttachmentData:[NSData dataWithContentsOfURL:url]
-                                       typeIdentifier:typeIdentifier
-                                             filename:filename]) {
-                                NSLog(@"attachment failed to add: %@", attachment);
+        if(args[@"messageText"])
+        {
+            mcvc.body = [RCTConvert NSString:args[@"messageText"]];
+        }
+
+        if(args[@"presentAnimated"])
+        {
+            presentAnimated = [RCTConvert BOOL:args[@"presentAnimated"]];
+        }
+
+        if(args[@"dismissAnimated"])
+        {
+            dismissAnimated = [RCTConvert BOOL:args[@"dismissAnimated"]];
+        }
+
+        if([MFMessageComposeViewController canSendAttachments]) {
+            if(args[@"attachments"])
+            {
+                if([args[@"attachments"] isKindOfClass:[NSArray class]])
+                {
+                    NSArray *attachments = args[@"attachments"];
+                    for(id attachment in attachments)
+                    {
+                        if([attachment isKindOfClass:[NSDictionary class]])
+                        {
+                            if ([attachment objectForKey:@"url"] && [attachment objectForKey:@"typeIdentifier"])
+                            {
+                                NSURL *url = [NSURL URLWithString:[attachment objectForKey:@"url"]];
+                                NSString *typeIdentifier = [attachment objectForKey:@"typeIdentifier"];
+                                NSString *filename = [attachment objectForKey:@"filename"];
+
+                                if (![mcvc addAttachmentData:[NSData dataWithContentsOfURL:url]
+                                           typeIdentifier:typeIdentifier
+                                                 filename:filename]) {
+                                    NSLog(@"attachment failed to add: %@", attachment);
+                                }
                             }
                         }
                     }
                 }
             }
         }
-    }
 
-    UIViewController *root = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
-    while (root.presentedViewController) {
-        root = root.presentedViewController;
-    }
-    [root presentViewController:mcvc animated:presentAnimated completion:nil];
+        UIViewController *root = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+        while (root.presentedViewController) {
+            root = root.presentedViewController;
+        }
 
-    [composeViews addObject:mcvc];
-    [composeCallbacks addObject:callback];
+        [root presentViewController:mcvc animated:presentAnimated completion:nil];
+
+        [composeViews addObject:mcvc];
+        [composeCallbacks addObject:callback];
+
+    });
+
 }
 
 #pragma mark - MFMessageComposeViewControllerDelegate
@@ -191,12 +196,12 @@ RCT_EXPORT_METHOD(composeMessageWithArgs:(NSDictionary *)args callback:(RCTRespo
             default:
                 break;
         }
-        
+
     }];
-    
+
     [composeViews removeObjectAtIndex:index];
     [composeCallbacks removeObjectAtIndex:index];
-    
+
 }
 
 @end
